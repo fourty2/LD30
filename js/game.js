69ff86118c19637131f4787c8e8414786f5012a4
Marc Henklein <snooze82@gmail.com>
2014-08-23 16:52:25 +0200

    key and door, basic collision detection

--- js/game.js
+++ js/game.js
@@ -5,6 +5,7 @@ var ld30 = {
 	player: null,
 	scene: null,
 	mousePos: null,
+	collidableMeshes: [],
 	init:  function() {
 		var WIDTH = window.innerWidth;
 		var HEIGHT = window.innerHeight;
@@ -57,11 +58,11 @@ var ld30 = {
 
 		// level
 		// 16 * 16
-		var testLevel = [
+		this.testLevel = [
 			[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 			[1,0,0,0,1,0,0,0,0,0,0,0,1,0,0,1],
-			[1,0,0,0,1,0,0,0,0,0,0,0,1,0,0,1],
-			[1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1],
+			[1,0,3,0,1,0,0,0,0,0,0,0,1,0,0,1],
+			[1,4,4,4,1,0,0,0,0,0,0,0,0,0,0,1],
 			[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
 			[1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1],
 			[1,0,0,0,0,0,0,1,1,1,0,0,0,0,0,1],
@@ -75,11 +76,25 @@ var ld30 = {
 			[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
 			[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 
-		]
+		];
+
+		this.collidableMeshes = this.testLevel;
+
+		this.testRays = [
+		  new THREE.Vector3(0, 0, 1),
+		  new THREE.Vector3(1, 0, 1),
+		  new THREE.Vector3(1, 0, 0),
+		  new THREE.Vector3(1, 0, -1),
+		  new THREE.Vector3(0, 0, -1),
+		  new THREE.Vector3(-1, 0, -1),
+		  new THREE.Vector3(-1, 0, 0),
+		  new THREE.Vector3(-1, 0, 1)
+		];
+		this.caster = new THREE.Raycaster();	
 
 		for (var y=0;y<=15;y++) {
 			for (var x=0;x<=15;x++) {
-				switch (testLevel[y][x]) {
+				switch (this.testLevel[y][x]) {
 					case 1:
 						blockMesh = new THREE.Mesh(
 							new THREE.CubeGeometry(50,50,50),
@@ -90,12 +105,48 @@ var ld30 = {
 						blockMesh.position.x = -375 + (x*50);
 						blockMesh.position.z = -375 + (y*50);
 
+						this.collidableMeshes[y][x] = blockMesh;
 						this.scene.add(blockMesh); 
 					break;
 					case 2:
 						this.playerStartPos = {x: -375 + (x*50), z:-375 + (y*50)};
 					break;
+					case 3:
+						// schlüssel
+						blockMesh = new THREE.Mesh(
+							new THREE.CubeGeometry(20,50,4),
+							new THREE.MeshLambertMaterial({color: 0x0000ff})
+						);
+						blockMesh.castShadow = true;
+						blockMesh.receiveShadow = true;
+						blockMesh.position.x = -375 + (x*50);
+						blockMesh.position.z = -375 + (y*50);
+						this.scene.add(blockMesh); 
+					break;
+					case 4:
+						// mauer
+						blockMesh = new THREE.Mesh(
+							new THREE.CubeGeometry(50,15,4),
+							new THREE.MeshLambertMaterial({color: 0xff0077})
+						);
+						blockMesh.castShadow = true;
+						blockMesh.receiveShadow = true;
+						blockMesh.position.x = -375 + (x*50);
+						blockMesh.position.z = -375 + (y*50);
+						this.scene.add(blockMesh); 
+						blockMesh = new THREE.Mesh(
+							new THREE.CubeGeometry(50,7,4),
+							new THREE.MeshLambertMaterial({color: 0xff0077})
+						);
+						blockMesh.castShadow = true;
+						blockMesh.receiveShadow = true;
+						blockMesh.position.x = -375 + (x*50);
+						blockMesh.position.z = -375 + (y*50);
 
+						blockMesh.position.y = 20;
+						this.scene.add(blockMesh); 
+						
+					break;
 
 				}
 			}
@@ -146,8 +197,51 @@ var ld30 = {
 		document.addEventListener( 'keyup', this.onKeyUp, false );
 
 	},
+	checkCollision: function() {
+		mapx = (this.player.position.x + 375)/50;
+		mapy = (this.player.position.z + 375)/50;
+		fmapx = Math.floor(mapx);
+		cmapx = Math.ceil(mapx);
+		fmapy = Math.floor(mapy);
+		cmapy = Math.ceil(mapy);
+	
+
+		var preCollisionList = [
+			this.collidableMeshes[fmapy][fmapx],
+			this.collidableMeshes[cmapy][fmapx],
+			this.collidableMeshes[fmapy][cmapx],
+			this.collidableMeshes[cmapy][cmapx],
+		];
+		var collisionList = [];
+		preCollisionList.forEach(function(elem) {
+			if (!Number.isInteger(elem)) {
+				collisionList.push(elem);
+			}
+		});
+		if (collisionList.length == 0) {
+			return false;
+		}
+	
+		// raycaster starten
+		for (i=0;i<this.testRays.length;i++) {
+			this.caster.set(this.player.position, this.testRays[i]);
+			collisions = this.caster.intersectObjects(collisionList);
+			if (collisions.length > 0 && collisions[0].distance <= 5) {
+				return true;
+			}
+
+		}
+
+		return false;
+	},
 	render: function() {
-		this.controls.update(0.016); // ??
+		var oldPosition = this.player.position.clone();
+		this.controls.update(0.016); 
+		if (this.checkCollision()) {
+			this.player.position.set(oldPosition.x, oldPosition.y, oldPosition.z);
+		}
+		oldPosition = null;
+
 		this.camera.update();
 		this.renderer.render(this.scene, this.camera);
 
@@ -166,11 +260,11 @@ var ld30 = {
 	        x = 0;
 	    }
 
-	    if( (y > 0 && y < threshold) || (y < 0 && y > -threshold) ) {
+	  /*  if( (y > 0 && y < threshold) || (y < 0 && y > -threshold) ) {
 	        y = 0;
 	    }
-	
-	    ld30.mousePos.set( x, 0 * y );
+	*/
+	    ld30.mousePos.set( x, 0);
 	},
 
 	onKeyDown: function( e ) {
